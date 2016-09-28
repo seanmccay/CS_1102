397e41a9fd8766fbd363b37cccbe508f046cd50e
seanmccay <seanmccay.development@gmail.com>
2016-09-28 10:57:17 -0500

    Completed Assignment for Unit 4

---
+++ src/TestOtherCode.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 9/22/2016.
+ */
+public class TestOtherCode {
+        public static void main(String[] args) {
+              //some code that I want to test when assessing other students' assignments
+        }
+}
